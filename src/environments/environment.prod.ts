ccdcc90f44fe8e937d0b4a07731c6692a03c21cc
VMTrade <53377788+VMTrade@users.noreply.github.com>
2020-06-17 03:38:51 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL:"https://git.heroku.com/backcamp.git/universityApi/",
+  apiURL:"https://backcamp.herokuapp.com/universityApi/",
   facultySecurityCode:'true_faculty',
   mApiKey:'AIzaSyCDKBu1aPoiFQX0tCZUJJ2I8_JRW7f_vmU'
 };
